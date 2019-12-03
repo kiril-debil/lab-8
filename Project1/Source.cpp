71f8231eafa939edb67e7af421b1b42b854eab43
kiril-debil <k.githubovich@gmail.com>
2019-12-03 14:30:56 +0200

    initial commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+#include "windows.h"
+#include "math.h"
+
+
+using namespace std;
+
+int main()
+{
+
+	int y;
+
+	cout << "y=";
+	cin >> y;
+
+	int z;
+
+	
+	z = 34634 * y;
+	cout << "z=" << z << "\n";
+	system("pause");
+	return 0;
+}
